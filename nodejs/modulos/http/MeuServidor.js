7b1de9e8d6091e64f833714e493890403f0fac8f
Mateus de Aguiar Guimarães <mateusag2007@gmail.com>
2025-10-10 11:00:09 -0300

    express generator

--- nodejs/modulos/http/MeuServidor.js
+++ nodejs/modulos/http/MeuServidor.js
@@ -1,29 +1,21 @@
-const http = require("http");
-
-const PORT = 3000;
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/plain; charset=utf-8");
-
-  // Controle de rotas
-  if (req.url === "/" && req.method === "GET") {
-    res.statusCode = 200;
-    res.end("Página inicial");
-  } 
-  else if (req.url === "/sobre" && req.method === "GET") {
-    res.statusCode = 200;
-    res.end("Página sobre");
-  } 
-  else if (req.url === "/contato" && req.method === "POST") {
-    res.statusCode = 200;
-    res.end("Formulário de contato enviado!");
-  } 
-  else {
-    res.statusCode = 404;
-    res.end("Rota não encontrada");
-  }
+    if (req.url === '/' && req.method === 'GET') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Rota principal');
+    } else if (req.url === '/sobre' && req.method === 'GET') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Rota sobre');
+    } else if (req.url === '/contato' && req.method === 'GET') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Rota contato');
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Rota não encontrada');
+    }
 });
 
-server.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+server.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
 });
