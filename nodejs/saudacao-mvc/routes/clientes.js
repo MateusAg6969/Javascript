9e014b636f0d257788387512a7e745dc2089a7bb
Mateus de Aguiar Guimarães <mateusag2007@gmail.com>
2025-10-24 08:49:35 -0300

    Versão login completa

--- nodejs/saudacao-mvc/routes/clientes.js
+++ nodejs/saudacao-mvc/routes/clientes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const clientesController = require('../controllers/clientesController');
 
 router.get('/', clientesController.clientes);
+router.get('/login', clientesController.login);
 
 module.exports = router;
